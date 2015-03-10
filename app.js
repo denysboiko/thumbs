b229d6ec956893a78f1183dbcc6de582c515323e
denysboiko <denys.bojko@gmail.com>
2015-03-10 20:11:28 +0200

    zip and download

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({secret: 'ssshhhhh'}));
+//app.use(session({secret: 'ssshhhhh'}));
 
 app.use('/', routes);
 app.use('/users', users);
